dad269c4cd43f2d13a33f1beb7a3d2342f2f363f
melissaKG <mkaufmangomez@gmail.com>
2016-10-28 19:00:57 -0400

    SOMETHING PRINTS

--- prettyprint.ml
+++ prettyprint.ml
@@ -11,8 +11,13 @@ let string_of_op = function
 let string_of_expr = function
 	Lit(l) -> string_of_int l
 
-(*let _ =
-	let lexbuf = Lexing.from_channel stdin in
-	let expr = Parser.expr Scanner.token lexbuf in
-	let result = (* pretty printing function name *) expr in 
-	print_endline (string_of_int result) *)
+let string_of_typ = function
+	Int -> "int"
+  | Bool -> "bool"
+  | Void -> "void"
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_prog =
+	(*print_endline(String.concat "" (List.map string_of_vdecl vars)) *)
+	print_endline ("hello")
