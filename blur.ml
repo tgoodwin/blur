0872c9b72772cb64b5abb6275c4e950ccadd156e
melissaKG <mkaufmangomez@gmail.com>
2016-11-01 14:54:29 -0400

    Need to fix this, but working toward pretty printing.

--- blur.ml
+++ blur.ml
@@ -2,5 +2,5 @@ open Ast
 open Prettyprint
 
 let lexbuf = Lexing.from_channel stdin;;
-let ast = Parser.program Scanner.token lexbuf;
-print_endline ("pretty print blur");;
+let ast = Parser.program Scanner.token lexbuf;;
+print_endline (Prettyprint.string_of_prog ast);;
