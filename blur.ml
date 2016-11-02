f562778b2488a196a0c6f4a2fe646a29714a763e
melissaKG <mkaufmangomez@gmail.com>
2016-11-02 16:59:36 -0400

    Pretty print can take a file as an arg.

--- blur.ml
+++ blur.ml
@@ -3,4 +3,4 @@ open Prettyprint
 
 let lexbuf = Lexing.from_channel stdin;;
 let ast = Parser.program Scanner.token lexbuf;;
-Prettyprint.string_of_prog;;
+print_endline (Prettyprint.string_of_prog ast);;
--- prettyprint.ml
+++ prettyprint.ml
@@ -11,6 +11,15 @@ let string_of_op = function
 let string_of_expr = function
 	  Lit(l) -> string_of_int l
 
+let rec string_of_expr = function
+    Lit(l) -> string_of_int l
+  | Id(s) -> s
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n"   
+
 let string_of_typ = function
 	  Int -> "int"
   | Bool -> "bool"
@@ -18,6 +27,12 @@ let string_of_typ = function
 
 let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
-let string_of_prog =
-	(*print_endline(String.concat "" (List.map string_of_vdecl vars)) *)
-	print_endline ("hello")
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^ "}\n"
+
+let string_of_prog (vars, func)  =
+	String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl func)
