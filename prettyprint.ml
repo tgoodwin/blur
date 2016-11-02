71ccde2e7f57466f2503f79cf44957015a8f0eb5
tim goodwin <tlg2132@columbia.edu>
2016-11-01 23:47:15 -0400

    fleshing out pretty printing, working back down the tree

--- prettyprint.ml
+++ prettyprint.ml
@@ -18,6 +18,21 @@ let string_of_typ = function
 
 let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
-let string_of_prog =
+(* calls the individual print functinos of the data members of a variable_Declaration type *)
+let print_string_of_var_decl vd = function
+    _ -> "dummy"
+
+(* this will handle all the data members of the function type call their print functions individually *)
+let print_string_of_func_decl fd = function
+    _ -> "fun dummy"
+
+let print_string_of_decl = function
+    VarDecl(vd) -> print_string_of_var_decl vd
+  | FuncDecl(fd) -> print_string_of_func_decl fd
+
+let print_string_of_program prog =
+    List.map print_string_of_decls prog 
+
+let string_of_prog prog =
 	(*print_endline(String.concat "" (List.map string_of_vdecl vars)) *)
 	print_endline ("hello")
