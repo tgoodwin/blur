89bf77e35cf225a4971e4333144cfd52e61da734
restocle <sh3266@columbia.edu>
2016-11-10 17:53:08 -0500

    testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,39 @@
 
 # bash script to read & test text files in tests folder through LLVM
 
+# Path to LLVM
+LLI="lli"
+
+# BLUR compiler
+BLUR="./blur.native"
+
+# operation time limit
+ulimit -t 30
+
+Usage() {
+    echo "Usage: testall.sh"
+    echo "-s    Test single case"
+    echo "-a    Test all"
+    exit 1
+}
+
+SignalError() {
+    if [ $error -eq 0 ] ; then
+	echo "FAILED"
+	error=1
+    fi
+    echo " $1"
+}
+
+# Compare <file1> <file2> <diff>
+# compares file1 and file2, writes difference to diff
+Compare() {
+    generated="$generated $3"
+    echo diff -b $1 $2 ">" $3 1>&2
+    diff -b "$1" "$2" > "$3" 2>$1 || {
+	SignalError "$1 differs"
+	echo "FAILED $1 differs from $2 " 1>&2
+    }
+}
+
 
