01224bc8fe637fedc45dfafc626aea20c47dfa9f
melissaKG <mkaufmangomez@gmail.com>
2016-11-13 11:59:56 -0500

    Checking for duplicate function names.

--- semantic_analyzer.ml
+++ semantic_analyzer.ml
@@ -24,9 +24,20 @@ let check_prog (globals, functions) =
 	(* Raise exception if given list has a duplicate *)
 	
 	(* There may not be duplicate variable names. *)
-	let report_duplicate exceptf globals =
+	let report_duplicate_var exceptf globals =
 		(* Get the names of the globals *)
 		let global_names = List.map (fun v -> v.declID) globals in
+			let rec helper = function	
+					n1 :: n2 :: _ when n1 = n2 -> raise (Failure (exceptf n1))
+				| _ :: t -> helper t
+				| [] -> ()
+			in helper (List.sort compare global_names)
+	in
+
+	(* There may not be duplicate function names. *)
+	let report_duplicate_func exceptf globals =
+		(* Get the names of the globals *)
+		let global_names = List.map (fun f -> f.fname) globals in
 			let rec helper = function	
 					n1 :: n2 :: _ when n1 = n2 -> raise (Failure (exceptf n1))
 				| _ :: t -> helper t
@@ -40,8 +51,19 @@ let check_prog (globals, functions) =
 		(* Get the types of the globals *)
 		let global_typ = (fun v -> v.declTyp) vdecl in
 				if global_typ = Void then raise (Failure ("illegal global var"))
-				else () in
+				else () 
+	in
+
+	(* Check assignment - type error *)
+
+
+	(**** Check global vars ****)
 
 	List.iter check_not_void globals;
 
-	report_duplicate (fun n -> "duplicate global " ^ n) globals;
+	report_duplicate_var (fun n -> "duplicate global " ^ n) globals;
+
+	(**** Check functions ****)
+
+	report_duplicate_func (fun n -> "duplicate function " ^ n) functions;
+
