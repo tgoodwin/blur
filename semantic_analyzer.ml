ee4472681830d0258d9fbab4295fc4fd9d003672
melissaKG <mkaufmangomez@gmail.com>
2016-12-06 15:55:19 -0500

    Updating sem anal.

--- semantic_analyzer.ml
+++ semantic_analyzer.ml
@@ -8,24 +8,320 @@ module StringMap = Map.Make(String)
 
 type symbol_table = {
     parent: symbol_table option;
+    args: argdecl list;
     variables: vardecl list
 }
 
-(* see slide 73 of types lecture *)
-type func_type = {
+type func_entry = {
     name: string;
-    input_types: datatype list;
+    arg_types: datatype list;
     return_type: datatype
 }
 
-type translation_env = {
-    scope: symbol_table;
-    functions: func_type list;
-    return_type: datatype option;
+type env = {
+    symtab: symbol_table;
+    funcs: func_entry list;
+    return_type: datatype option
 }
 
+let string_of_typ = function
+    Int -> "int"
+  | Double -> "double"
+  | Char -> "char"
+  | String -> "string"
+  | Bool -> "bool"
+  | Void -> "void"
+
+let string_of_datatype = function 
+  | Datatype(t) -> string_of_typ t
+
 let check_prog (globals, functions) = 
-	(* Make a map of global variables *)
+	(* Add global variable declarations to the symbol table *)
+	print_endline("checking prog");
+
+	let built_in_functions = 
+		(* TODO: print takes an expr, not an string. See if we need to change this later. *)
+		[ {name = "print"; arg_types = [Datatype(String)]; return_type = Datatype(Void);} ]
+	in
+
+	(* A global variable cannot have type void. *)
+	let check_not_void (vdecl : vardecl) = 
+		(* Get the types of the globals *)
+		let global_typ = (fun v -> v.declTyp) vdecl in
+				if global_typ = Datatype(Void) then raise 
+					(Failure ("illegal void variable " ^  vdecl.declID))
+				else () 
+	in
+	List.iter check_not_void globals;
+
+	let rec get_variable_decl (symtab : symbol_table) (id : string) :vardecl =
+		print_endline("getting var decl");
+		print_endline(string_of_int(List.length symtab.variables));
+		try List.find (fun vdecl -> vdecl.declID = id) symtab.variables
+	  with
+	  | Not_found -> 
+	  	(match symtab.parent with
+	  		| Some parent -> print_endline("look at parent"); get_variable_decl parent id 
+	  		| _ -> raise Not_found) in
+	let get_variable_type (symtab : symbol_table) (id : string) :datatype =
+		print_endline("getting var type");
+		let vdecl = get_variable_decl symtab id
+	  in vdecl.declTyp in
+	let check_variable_declaration (env : env) (decl: vardecl) = 
+		print_endline("checking var decls");
+
+		(* A variable cannot have type void. *)
+		let check_not_void_var (decl : vardecl) = 
+			print_endline("checking void vars");
+			let var_typ = (fun v -> decl.declTyp) decl in
+					if var_typ = Datatype(Void) then raise (Failure ("illegal void variable " ^ decl.declID))
+					else ()
+		in 
+		ignore(check_not_void_var (decl));
+
+		(* Ensure that declInit and declType match using check_expr *)
+		(try
+			let _ = 
+				(* Error out if local variable with same name already exists. *)
+				List.find 
+					(fun vdecl -> vdecl.declID = decl.declID) env.symtab.variables
+			in raise (Failure ("Duplicate variable " ^ decl.declID))
+		with
+		| Not_found -> 
+			(* TODO: use same symbol table as symbol table from arg *)
+			let new_symbol_table = 
+				{
+					(env.symtab)
+					with 
+					variables = decl :: env.symtab.variables;
+				} in
+			let new_env = { (env) with symtab = new_symbol_table; }
+			and vdecl = 
+				{
+					declTyp = decl.declTyp;
+					declID = decl.declID;
+					declInit = decl.declInit;
+				}
+			in (new_env, vdecl))
+	in	
+
+	(* Check arguments *)
+	let check_argdecl (env : env) (adecl : argdecl) = 
+		print_endline("checking arg decl");
+
+		(* An argument cannot have type void. *)
+		let check_not_void_arg (adecl : argdecl) = 
+			print_endline("checking void args");
+			let arg_typ = (fun a -> a.argdeclType) adecl in
+					if arg_typ = Datatype(Void) then raise (Failure ("illegal void arg"))
+					else ()
+		in 
+		ignore(check_not_void_arg (adecl));
+
+		(try
+			let _ = 
+				(* Error out if local variable with same name already exists. *)
+				List.find 
+					(fun argdecl -> argdecl.argdeclID = adecl.argdeclID) env.symtab.args
+			in raise (Failure ("Duplicate variable " ^ adecl.argdeclID))
+		with
+		| Not_found -> 
+			let new_symbol_table = 
+				{
+					(env.symtab)
+					with 
+					args = adecl :: env.symtab.args;
+				} in
+			let new_env = { (env) with symtab = new_symbol_table; }
+			and arg = 
+				{
+					argdeclType = adecl.argdeclType;
+					argdeclID = adecl.argdeclID;
+				}
+			in (new_env, adecl))
+	in
+
+	(* Checking function call returns the type of the function. *)
+	let check_func_call (id : string) (args : expr list) (env : env) = 
+		print_endline("checking function call");
+		print_endline(string_of_int(List.length env.funcs));
+		try
+			let func_entry = List.find (fun f -> f.name = id) env.funcs in
+			Datatype(Int)
+		with | Not_found -> raise (Failure ("undeclared function " ^ id)) 
+	in
+
+	(* Returns type of expression. *)
+	let rec check_expr (env : env) (expr : expr) = 
+		match expr with 
+			IntLit i -> print_endline("int"); Datatype(Int)
+		| DoubleLit d -> print_endline("double"); Datatype(Double)
+		| Id s -> print_endline("id"); 
+				(try get_variable_type env.symtab s 
+				with | Not_found -> raise (Failure ("undeclared identifier " ^ s))
+				) 
+			(*Datatype(Int)*) (* Get type of var*)
+		| FuncCall (s, arglist) -> check_func_call s arglist env
+		| Binop (e1, op, e2) -> print_endline("expr is binop");
+			let t1 = check_expr env e1 
+			and t2 = check_expr env e2 in
+			match op with 
+			| Add | Sub | Mult | Div -> print_endline("arith");
+			ignore(print_endline(string_of_datatype t1));
+			ignore(print_endline(string_of_datatype t1));
+			if t1 <> t2 then raise (Failure ("illegal operation")) 
+			else t1
+			(* TODO: fail if type is not int or double *)
+			| Asn -> print_endline("asn");
+				if t1 = t2 then t1
+				else raise (Failure ("illegal assignment")) 
+	in
+
+	(* Return env and stmt tuple. *)
+	let check_stmt (env : env) (stmt : stmt) :(env * stmt) = 
+		print_endline("checking stmt");
+		match stmt with 
+			Expr e -> ignore(check_expr env e); (env, stmt) (* Expression cannot mutate the environment. *)
+		| Decl vdecl -> (* Return new env*)
+			let (new_env, vdecl) = check_variable_declaration env vdecl
+			in (new_env, stmt)
+		| Return e -> let e_type = check_expr env e in
+			(match env.return_type with
+				| Some return_type ->
+					if e_type = return_type then (env, stmt)
+					else raise (Failure ("incorrect return type"))
+					| None -> raise (Failure ("no return")))
+	in
+
+	(* Each statement takes the environment updated from the previous statement. *)
+	let check_stmt_list (env : env) ( slist : stmt list ) :(env * stmt list) = 
+		print_endline("checking stmt list");
+		let(new_env, stmts) = 
+			List.fold_left (fun acc stmt ->
+				let (nenv, s) = check_stmt (fst acc) stmt
+			  in (nenv, (s :: (snd acc)))) (env, []) slist
+		in (new_env, List.rev stmts) 
+	in
+
+	(* Check function declaration and return new environment. *)
+	let check_function_declaration (env : env) (fdecl : funcdecl) : (env * funcdecl) =
+		print_endline("checking func decl");
+		print_endline(string_of_int(List.length env.symtab.variables));
+		if (List.mem fdecl.fname (List.map (fun f -> f.name) built_in_functions)) then
+		raise (Failure ("Cannot overwrite print function!!")) else
+		(* Get the types of the function's arguments. *)
+		let a_types = List.map (fun adecl -> adecl.argdeclType) fdecl.args in
+		(* Make a function entry for the function. *)
+		let func_entry = 
+			{
+				name = fdecl.fname;
+				arg_types = a_types;
+				return_type = fdecl.typ;
+			} in
+		let new_funcs = func_entry :: env.funcs in
+		(* Make a new symbol table for the function scope. *)
+		let new_symbol_table = 
+			{
+				parent = Some env.symtab;
+				args = [];
+				variables = [];
+			} in
+		(* Add the function to the environment 
+		For now, the symbol table and return type have empty local scope. *)
+		let new_env = 
+		{
+			(env)
+			with
+			symtab = new_symbol_table;
+			funcs =  new_funcs;
+			return_type = Some fdecl.typ;
+		} in
+		print_endline("func count:");
+		print_endline(string_of_int(List.length new_env.funcs));
+		(* Add the args to the function scope. *)
+		let (env_with_args, argdecl_list) = 
+			List.fold_left (fun acc argdecl ->
+				let (nenv, arg) = check_argdecl (fst acc) argdecl 
+				in (nenv, (arg :: (snd acc)))) (new_env, []) fdecl.args in
+		(* No need to keep track of environment outside the scope of the function. *)
+		let (_, func_body) = 
+			check_stmt_list env_with_args fdecl.body in 
+		let func_body = func_body in
+		(* Return the environment with this added function. *)
+		({ (env) with funcs = new_funcs; }, fdecl) 
+  	in
+
+
+	(* Establish initial environment *)
+	let env = 
+		{
+			symtab = { parent = None; variables = []; args = []; };
+			funcs = built_in_functions; 
+			return_type = None;
+		} in
+
+	(* Add global variables to the environment. *)
+	let check_global_var (env : env) (vdecl : vardecl) = 
+		print_endline("checking global vars");
+		(try
+			let _ =	
+				(* Error out if global variable with same name already exists. *)
+				List.find 
+					(fun v -> v.declID = vdecl.declID) env.symtab.variables
+			in raise (Failure ("Duplicate variable " ^ vdecl.declID))
+		with
+			| Not_found -> 
+				print_endline("add global to symbol tab");
+				let new_symbol_table = 
+					print_endline(string_of_int(List.length env.symtab.variables));
+					{
+						(env.symtab)
+						with 
+						variables = vdecl :: env.symtab.variables;
+					} in
+				let new_env = { (env) with symtab = new_symbol_table; }
+				and vardecl = 
+					{
+						declTyp = vdecl.declTyp;
+						declID = vdecl.declID;
+						declInit = vdecl.declInit;
+					}
+				in (new_env, vardecl))
+	in
+
+	let(new_env, vars) = 
+		print_endline("globals loop");
+		List.fold_left (fun acc v ->
+			let (nenv, v) = check_global_var (fst acc) v
+			in (nenv, (v :: (snd acc)))) (env, []) globals 
+	in 
+
+	print_endline("after globals run");
+	print_endline(string_of_int(List.length new_env.symtab.variables));
+
+	let (_, fdecl_list) = 
+		print_endline("function list size");
+		print_endline(string_of_int(List.length functions));
+		List.fold_left (fun acc fdecl ->
+			print_endline("folding");
+			print_endline(fdecl.fname);
+			let (new_env, f) = check_function_declaration (fst acc) fdecl
+			in (new_env, (f :: (snd acc)))) (new_env, []) functions
+	in fdecl_list;
+
+	let check_function functions =  
+		print_endline("checking functions");
+
+		(* Return list of functions after checking functions. *)
+		functions in
+
+		(* After semantically checking, we return the program -
+		a tuple of a list of globals and a list of functions. *)
+		(globals, functions);
+
+		(*in List.iter check_function functions*)
+
+	(*(* Make a map of global variables *)
 	let global_vars = 
 		let global_var map (vdecl : A.vardecl) =
 			let name = vdecl.declID in
@@ -121,7 +417,7 @@ let check_prog (globals, functions) =
 
   let _ = function_decl "main" in (* Ensure "main" is defined. *)
 
-	let check_function func =
+	let check_function func env =
 		List.iter check_not_void_arg func.args;
 
 		report_duplicate_arg (fun n -> "duplicate argument " ^ n) func.args;
@@ -143,12 +439,22 @@ let check_prog (globals, functions) =
 		let local_vars = List.fold_left add_arg local_vars func.args in
 
 		let type_of_var v =
+			print_endline("decl?");
 			try StringMap.find v local_vars
 			with Not_found -> raise (Failure ("undeclared identifier " ^ v))
 		in
 		
 		let rec binop typed_e1 op typed_e2 = 
 			check_assign typed_e1 typed_e2 (Failure ("illegal assignment"))
+		in
+
+		let check_local_vardecl vdecl map = 
+			let local_vars = add_local map vdecl in
+
+      let init_expr = vdecl.declInit in
+      match init_expr with
+        A.Noexpr      -> local_vars
+      | e             -> local_vars
 		in
 
 		(* Return the type of an expression or throw exception. *)
@@ -166,19 +472,23 @@ let check_prog (globals, functions) =
 		in
 
 		let rec stmt = function
-				Expr e -> ignore (expr e)
+				Expr e -> ignore (expr e) ; local_vars
+			| Decl vardecl -> 
+				(*let local_vars = check_local_vardecl vardecl map
+				in local_vars*)check_local_vardecl vardecl local_vars ; local_vars
 			|	Block stmtlst -> let rec check_block = function
 						[Return _ as s] -> stmt s
-					| Return _ :: _ -> raise (Failure "nothing may follow a return")
-					| Block sl :: ss -> check_block (sl @ ss)
-					| s :: ss -> stmt s ; check_block ss
-					| [] -> ()
+					| Return _ :: _ -> raise (Failure "nothing may follow a return") 
+					| Block sl :: ss -> check_block (sl @ ss) 
+					| s :: ss -> stmt s ; check_block ss 
+					| [] -> local_vars
 				in check_block stmtlst
 
 		in
 
 		(* Account for local var decls being part of func body stmt *)
-		stmt (Block func.body)
+		let _ = stmt (Block func.body) in ()
+
 
 	in
-	List.iter check_function functions
+	List.iter check_function functions*)
