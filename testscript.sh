fb879cfdee49714d60823382d332ec4438b13a40
restocle <sh3266@columbia.edu>
2016-12-18 13:23:54 -0500

    testscript update

--- testscript.sh
+++ testscript.sh
@@ -2,8 +2,8 @@
 
 code(){
     filebase=$(echo ${1} | cut -f 1 -d '.')
-	make "${filebase}-ls"
-    { ./${filebase}.blx; } &> output.txt
+	make "${filebase}-ls" &> makeOutput.txt
+    ./${filebase}.blx &> output.txt
     #cmp --silent output.txt helloWorld.out || echo "Wrong Output"
     DIFF=$(diff -bBw output.txt "${filebase}.out")
     if [ "$DIFF" == "" ]; then
@@ -12,6 +12,8 @@ code(){
 	echo "${filebase}: Wrong Output"
     fi
     rm -rf tests/*.ll
+	rm -rf tests/*.s
+	rm -rf tests/*.blx
 }
 
 codeAll(){
